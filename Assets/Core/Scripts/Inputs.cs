441139175a633653b6ee11e5a72c7f2a692945c7
Alessandro Grigis <alessandrog@monade.io>
2024-07-23 22:30:27 +0200

    collisioni astrali

--- Assets/Core/Scripts/Inputs.cs
+++ Assets/Core/Scripts/Inputs.cs
@@ -7,6 +7,13 @@ public class Inputs : MonoBehaviour
 {
 
     private Movement movement;
+    private Vector2 nextDirection = Vector2.right;
+    [SerializeField]
+    float distance = 0.5f;
+    [SerializeField]
+    float width = 0.5f;
+    [SerializeField]
+    LayerMask layerMask = 1;
 
     void Start()
     {
@@ -15,28 +22,56 @@ public class Inputs : MonoBehaviour
     public void OnLeftInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed) {
-            movement.Direction = Vector2.left;
+            nextDirection = Vector2.left;
         }
     }
 
     public void OnRightInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed) {
-            movement.Direction = Vector2.right;
+            nextDirection = Vector2.right;
         }
     }
 
     public void OnUpInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed) {
-            movement.Direction = Vector2.up;
+            nextDirection = Vector2.up;
         }
     }
 
     public void OnDownInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed) {
-            movement.Direction = Vector2.down;
+            nextDirection = Vector2.down;
         }
+    }
+
+    public void Update()
+    {
+        if (CanGo(nextDirection)) {
+            movement.Direction = nextDirection;
+        }
+        // FIXME - PacMan si incastra...
+        // if (!CanGo(movement.Direction)) {
+        //     movement.Direction = Vector2.zero;
+        // }
+    }
+
+
+    private bool CanGo(Vector3 direction)
+    {
+        return !Physics2D.Linecast(
+            Vector3.Cross(direction * distance, Vector3.forward) * width + transform.position + direction * distance,
+            Vector3.Cross(direction * distance, Vector3.back) * width + transform.position + direction * distance,
+            layerMask);
+    }
+
+    void OnDrawGizmos() {
+        Vector3 d = movement.Direction;
+        Gizmos.color = CanGo(d) ? Color.green : Color.red;
+        Gizmos.DrawLine(
+            (Vector2)(Vector3.Cross(d * distance, Vector3.forward) * width + transform.position + d * distance),
+            (Vector2)(Vector3.Cross(d * distance, Vector3.back) * width + transform.position + d * distance));
     }
 }
