47ccebac999dde4b60c05d85ad2cb82d799b58bf
Andrea Massioli <massioli.andrea@gmail.com>
2024-07-23 21:40:56 +0200

    COMMITTA STOCA**O

--- Assets/Core/Pill/PillsSpawner.cs
+++ Assets/Core/Pill/PillsSpawner.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PillsSpawner : MonoBehaviour
@@ -13,26 +15,44 @@ public class PillsSpawner : MonoBehaviour
             Instantiate(pillPrefab, spawnPoint, Quaternion.identity);
         }
     }
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        foreach (var spawnPoint in spawnPoints)
+        {
+            Gizmos.DrawSphere(spawnPoint, 0.1f);
+        }
+    }
 }
 
 #if UNITY_EDITOR
 [UnityEditor.CustomEditor(typeof(PillsSpawner))]
 public class PillsSpawnerEditor : UnityEditor.Editor
 {
+    private Vector3 origin = Vector3.zero;
+    private float cellSize = 1;
+    private Vector2Int gridSize = new(10, 10);
+    
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         var spawner = (PillsSpawner) target;
+        
+        origin = EditorGUILayout.Vector3Field("Origin", origin);
+        cellSize = EditorGUILayout.FloatField("Gap", cellSize);
+        gridSize = EditorGUILayout.Vector2IntField("Grid size", gridSize);
+        
         if (GUILayout.Button("Generate grid"))
         {
             var spawnPoints = serializedObject.FindProperty("spawnPoints");
             spawnPoints.ClearArray();
-            for (var x = 0; x < 10; x++)
+            for (var x = 0f; x < gridSize.x * cellSize; x += cellSize)
             {
-                for (var y = 0; y < 10; y++)
+                for (var y = 0f; y < gridSize.y * cellSize; y += cellSize)
                 {
                     spawnPoints.InsertArrayElementAtIndex(0);
-                    spawnPoints.GetArrayElementAtIndex(0).vector3Value = new Vector3(x, y, 0);
+                    spawnPoints.GetArrayElementAtIndex(0).vector3Value = origin + new Vector3(x, y, 0);
                 }
             }
             serializedObject.ApplyModifiedProperties();
